18888d67ca6124a0c4be11eba80b8f65f4c5cfab
François <francois.ste@free.fr>
2020-04-16 20:02:47 +0200

    V1.3 suppression MY_RFM69_ENABLE_ENCRYPTION. ajout MY_RFM69_IRQ_PIN. limitation à 100 du % batterie. suppression de la valeur moyenne de l'humidité

--- MySensors-sonde-si7021.ino
+++ MySensors-sonde-si7021.ino
@@ -1,5 +1,9 @@
 /* Sketch with Si7021 and battery monitoring
  *  
+ *  cible
+ *  - ARDUINO PRO MINI 3.3v 8Mhz modifié (retrait régulateur, led alim)
+ *  - Optiboot external 1MHz BOD1.8V 4800baud
+ *  
  *  si VALUE_DEBUG est défini
  *    - mesure temp et hum toutes les 10 sec
  *    - envoi temp et hum toutes les 30 sec max
@@ -29,27 +33,35 @@
  *    - pin à la masse : bit à 0
  *    - si valeurs 63 (aucun pont de soudure), alors nodeID automatique
  *  
- *    
+ *  EVOLUTIONS
+*   V1.3  28/03/19
+*   - suppression MY_RFM69_ENABLE_ENCRYPTION
+*   - ajout MY_RFM69_IRQ_PIN
+ *  - limitation à 100 du % batterie
+ *  
+ *  
 */
 //#define   MY_DEBUG
 //#define   MY_DEBUG_VERBOSE_RFM69
 #define MY_BAUD_RATE 4800
 //#define DEBUG_ON            // Rain gauge specific debug messages.
 
-//#define VALUE_DEBUG OUI
+//#define VALUE_DEBUG
 
 // For RFM69
 #define   MY_RADIO_RFM69
 #define   MY_RFM69_FREQUENCY RFM69_868MHZ
 #define   MY_IS_RFM69HW
+#define   MY_RFM69_IRQ_PIN   2
 #define   MY_RFM69_NEW_DRIVER
+#define   MY_RFM69_TX_POWER_DBM (5)
 
 // chiffrement 
-#define   MY_RFM69_ENABLE_ENCRYPTION
-#define   MY_SECURITY_SIMPLE_PASSWD "x8Ig.R76FjKL;e"
+//#define   MY_RFM69_ENABLE_ENCRYPTION
+//#define   MY_SECURITY_SIMPLE_PASSWD "x8Ig.R76FjKL;e"
 
-#define SKETCH_NAME "Crakett - Si7021"
-#define SKETCH_VERSION "1.2"
+#define SKETCH_NAME "Sonde T&H - Si7021"
+#define SKETCH_VERSION "1.3"
 
 // ID static
 //#define NODE_ID 101             // <<<<<<<<<<<<<<<<<<<<<<<<<<<   Enter Node_ID
@@ -58,8 +70,6 @@
 #include <Wire.h>
 #include <SI7021.h>
 #include <SPI.h>
-#include <RunningAverage.h>
-
 
 #ifdef MY_DEBUG
 #define DEBUG_SERIAL(x) Serial.begin(x)
@@ -85,16 +95,15 @@
 #define CHILD_ID_TEMP 0
 #define CHILD_ID_HUM 1
 
-
 #ifdef VALUE_DEBUG
-#define SLEEP_TIME 10000             // 10000 ms ==> 10 sec
-#define FORCE_TRANSMIT_CYCLE 3       // 3*10 ==> 30 sec ; force l'envoi de la température et humudité toutes les 60 sec max 
-#define BATTERY_REPORT_CYCLE 3       // 3*10 ==> 30 sec ; une fois toutes les 30 sec
+#define SLEEP_TIME 2000              // 2000 ms ==> 2 sec
+#define FORCE_TRANSMIT_CYCLE 3       // 3*2 ==> 6 sec ; force l'envoi de la température et humudité toutes les 6 sec max 
+#define BATTERY_REPORT_CYCLE 5       // 5*2 ==> 10 sec ; une fois toutes les 10 sec
 #define HUMI_TRANSMIT_THRESHOLD 3.0  // seuil pour envoi humidité
 #define TEMP_TRANSMIT_THRESHOLD 0.2  // seuil pour envoi température
 #else
 #define SLEEP_TIME 120000            // 120000 ms ==> 120 sec ==> 2 min
-#define FORCE_TRANSMIT_CYCLE 5       // 5*2 ==> 10 min ; force l'envoi de la température et humudité toutes les 10 min max 
+#define FORCE_TRANSMIT_CYCLE 2       // 2*2 ==> 4 min ; force l'envoi de la température et humudité toutes les 4 min max 
 #define BATTERY_REPORT_CYCLE 180     // 180*2 ==> 360 min ; 360/60 ==> 6 h ; une fois toutes les 6h
 #define HUMI_TRANSMIT_THRESHOLD 3.0  // seuil pour envoi humidité
 #define TEMP_TRANSMIT_THRESHOLD 0.2  // seuil pour envoi température
@@ -104,19 +113,14 @@
 #define VMIN 1900
 #define VMAX 3000
 
-// moyenne humidité
-#define AVERAGES  2     
-
-int batteryReportCounter = BATTERY_REPORT_CYCLE - 1;  // to make it report the first time.
+int batteryReportCounter = BATTERY_REPORT_CYCLE;  // to make it report the first time.
 int measureCount = 0;
 float lastTemperature = -100;        // to make it report the first time.
 int lastHumidity = -100;             // to make it report the first time.
 
-RunningAverage raHum(AVERAGES);
-SI7021 humiditySensor;
+SI7021 tempEtHumSensor;
 
-
-MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP); // Initialize temperature message
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP); // Initialize temperature et humidité message
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 
 void setup() {
@@ -132,8 +136,6 @@ void setup() {
   DEBUG_PRINT("Node and ");
   DEBUG_PRINTLN("2 children presented.");
 
-  raHum.clear();
-
   pinMode(LED_PIN,OUTPUT);
   digitalWrite(LED_PIN,LOW);
 
@@ -143,6 +145,8 @@ void setup() {
 #else
   pulseLed(1);
 #endif
+
+ delay(1000); // attente avant 1er envoi temp/hum/batt
 
 }
 
@@ -158,7 +162,7 @@ void before()
     if(digitalRead(i) == HIGH)  valueID += 1 << (i-POID_FAIBLE);
     pinMode(i,INPUT);   // to reduce consommation : no pullup
   }
-  DEBUG_PRINT("nodeID by switch : "); DEBUG_PRINTLN(valueID);
+  DEBUG_PRINT(F("nodeID by switch : ")); DEBUG_PRINTLN(valueID);
   if(valueID != 63)  transportAssignNodeID(valueID);  // set switch nodeID (1 to 62) only if different of 63 decimal (not all bit to 1)
 }
 #endif
@@ -187,35 +191,45 @@ void loop() {
 void sendTempHumidityMeasurements() {
   
   bool forceTransmit = false;
-  int nbPulse = 1;     // 1 pulse si pas d'envoi (ni Temp ni Hum)
+  int nbPulse = 1;      // 1 pulse LED si pas d'envoi (ni Temp ni Hum)
   
   measureCount ++;
-  if (measureCount > FORCE_TRANSMIT_CYCLE)   forceTransmit = true;  // force la transmission
+  if (measureCount >= FORCE_TRANSMIT_CYCLE) {
+    forceTransmit = true;  // force la transmission
+    measureCount = 0;
+  }
   
-  si7021_thc data = humiditySensor.getTempAndRH();
+  si7021_thc data = tempEtHumSensor.getTempAndRH();
 
   float temperature = data.celsiusHundredths / 100.0;
   DEBUG_PRINT("T: "); DEBUG_PRINTLN(temperature);
   float diffTemp = abs(lastTemperature - temperature);
   DEBUG_PRINT(F("TempDiff :")); DEBUG_PRINTLN(diffTemp);
-  if (diffTemp > TEMP_TRANSMIT_THRESHOLD || forceTransmit) {
-    send(msgTemp.set(temperature, 1));
+  
+  if (diffTemp >= TEMP_TRANSMIT_THRESHOLD || forceTransmit) {
+    
+    if(!send(msgTemp.set(temperature, 1),false)) {
+      DEBUG_PRINTLN(F("ERREUR envoi température"));
+    }
+    
     lastTemperature = temperature;
-    measureCount = 0;
+    
     nbPulse += 1;                    // 2 pulse si envoi Temp
-    DEBUG_PRINTLN("T sent!");
+    DEBUG_PRINTLN(F("T sent!"));
   }
 
   int humidity = data.humidityPercent;
   DEBUG_PRINT("H: "); DEBUG_PRINTLN(humidity);
-  raHum.addValue(humidity);
-  humidity = raHum.getAverage();  // MA sample imply reasonable fast sample frequency
   float diffHum = abs(lastHumidity - humidity);
   DEBUG_PRINT(F("HumDiff  :")); DEBUG_PRINTLN(diffHum);
-  if (diffHum > HUMI_TRANSMIT_THRESHOLD || forceTransmit) {
-    send(msgHum.set(humidity));
+  
+  if (diffHum >= HUMI_TRANSMIT_THRESHOLD || forceTransmit) {
+
+    if(!send(msgHum.set(humidity),false)) {
+      DEBUG_PRINTLN(F("ERREUR envoi humidité"));
+    }
+    
     lastHumidity = humidity;
-    measureCount = 0;
     nbPulse += 2;                    // 3 pulse si envoi Hum, 4 pulse si envoi Temp + Hum
     DEBUG_PRINTLN("H sent!");
   }
@@ -240,11 +254,14 @@ void sendBatteryPercent() {
     DEBUG_PRINT("Battery voltage: ");
     DEBUG_PRINT(batteryVolt);
     DEBUG_PRINTLN(" mV");
-    uint8_t batteryPcnt = constrain(map(batteryVolt, VMIN, VMAX, 0, 100), 0, 255);
+    uint8_t batteryPcnt = constrain(map(batteryVolt, VMIN, VMAX, 0, 100), 0, 100);
     DEBUG_PRINT("Battery percent: ");
     DEBUG_PRINT(batteryPcnt);
     DEBUG_PRINTLN(" %");
-    sendBatteryLevel(batteryPcnt);
+    if(!sendBatteryLevel(batteryPcnt,false)) {
+      DEBUG_PRINTLN(F("ERREUR envoi niveau batterie"));
+    }
+    
     batteryReportCounter = 0;
   }
   
