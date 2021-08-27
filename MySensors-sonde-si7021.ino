531cc7225f55075123b3f8fbe4ca8d470af39fc9
François <francois.ste@free.fr>
2021-08-27 15:47:59 +0200

    new version (cf. readme)

--- MySensors-sonde-si7021.ino
+++ MySensors-sonde-si7021.ino
@@ -18,6 +18,7 @@
 #define   SKETCH_NAME "Sonde T et H - Si7021"
 #define   SKETCH_VERSION "1.7"
 
+#include <MyConfig.h>
 #include <MySensors.h>
 
 #include <Wire.h>
@@ -46,15 +47,13 @@
 #define HUMI_TRANSMIT_THRESHOLD 3.0  // seuil pour envoi humidité
 #define TEMP_TRANSMIT_THRESHOLD 0.2  // seuil pour envoi température
 #else
-#define SLEEP_TIME 120000            // 120000 ms ==> 120 sec ==> 2 min
-#define FORCE_TRANSMIT_CYCLE 5       // 5*2 ==> 10 min ; force l'envoi de la température et humudité toutes les 10 min max 
-#define BATTERY_REPORT_CYCLE 180     // 180*2 ==> 360 min ; 360/60 ==> 6 h ; une fois toutes les 6 h
-#define HUMI_TRANSMIT_THRESHOLD 3.0  // seuil pour envoi humidité
-#define TEMP_TRANSMIT_THRESHOLD 0.1  // seuil pour envoi température
+#define SLEEP_TIME 240000             // 240000 ms ==> 120 sec ==> 4 min ; mesure de la température et humidité toutes les 4 min
+#define FORCE_TRANSMIT_CYCLE 3       // 3*4 ==> 12 min ; force l'envoi de la température et humudité toutes les 12 min max (si n'a pas changé)
+#define BATTERY_REPORT_CYCLE 90      // 90*4 ==> 360 min ; 360/60 ==> 6 h ; envoi niveau batterie toutes les 6 h
+#define HUMI_TRANSMIT_THRESHOLD 3.0  // seuil d'écart humidité pour envoi humidité
+#define TEMP_TRANSMIT_THRESHOLD 0.1  // seuil d'écart température pour envoi température
+#define SAME_TEMP_THRESHOLD 30       // 30*4 ==> 120 min ; nombre de mesure de température identique pour déclencher panne sonde par envoi temp identique
 #endif
-
-#include <MyConfig.h>
-#include <MySensors.h>
 
 #ifdef MY_DEBUG_PRINT
 #define DEBUG_SERIAL(x) Serial.begin(x)
@@ -73,11 +72,18 @@ const int DELAI_TRANS = 200;       // 200 ms d'attente après chaque send()
 #define VMAX 3000
 
 int batteryReportCounter = BATTERY_REPORT_CYCLE;  // to make it report the first time.
-int measureCount = 0;
-float lastTemperature = -100;        // to make it report the first time.
-int lastHumidity = -100;             // to make it report the first time.
+int measureCount = FORCE_TRANSMIT_CYCLE;  // to make it report the first time.
 
 SI7021 tempEtHumSensor;
+
+float temperature;
+int humidity;
+float lastTemperature = 0; 
+int lastHumidity = 0;
+float lastTemperatureForCounter = 0;   // mesure précédente pour compter les mesure de température identique de la sonde 
+int sameTemperatureCounter=0;
+float deltaTemperature=0;              // 0.1 normalement. Si pendant SAME_TEMP_THRESHOLD la mesure de la température reste identique, alors 0
+float nextDeltaTemperature=0.1;        // 0.1 ou -0,1
 
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP); // Initialize temperature, humidité message et pile message
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
@@ -88,10 +94,11 @@ MyMessage msgBatt(CHILD_ID_BATT, V_VOLTAGE);
  * Setup code
  ***************************************************************************************************/
 
-void setup() {
+void setup()
+{
   //DEBUG_SERIAL(4800);    // <<<<<<<< Note BAUD_RATE in MySensors.h
 
-  delay(2500);    // délai à la mise sous tension
+  delay(4000);    // délai à la mise sous tension
 
   DEBUG_PRINTLN(F("Serial started"));
   DEBUG_PRINT(F("Voltage: "));
@@ -100,7 +107,6 @@ void setup() {
   
   pinMode(LED_PIN,OUTPUT);
   digitalWrite(LED_PIN,LOW);
-
 
 #ifdef VALUE_DEBUG
   pulseLed(2);
@@ -122,6 +128,8 @@ void before()
 {
   uint8_t i;
   uint8_t valueID=0;
+
+  delay(4000);    // délai à la mise sous tension
 
   for(i=POID_FAIBLE;i<POID_FORT;i++)
   {
@@ -151,69 +159,130 @@ void presentation()
 /***************************************************************************************************
  * Boucle principale
  ***************************************************************************************************/
-void loop() {
+void loop()
+{
+  ReadTempAndHumidity();
 
-  sendTempHumidityMeasurements();
+  // 1 pulse LED si pas d'envoi (ni Temp ni Hum)
+  int nbPulse = 1;
+  bool forceTransmit = IsForceTransmit();
+
+  DEBUG_PRINT(F("forceTransmit : ")); DEBUG_PRINTLN(forceTransmit);
+
+  nbPulse += SendTemperature(forceTransmit);  
+  nbPulse += SendHumidity(forceTransmit);
+
+  // pulse la LED
+  pulseLed(nbPulse);
 
   sendBatteryPercent();
 
   sleep(SLEEP_TIME);   // mise en mode très basse consommation
+
 }
 
-
 /***************************************************************************************************
- * Sends temperature and humidity from Si7021 sensor
+ * Mesure temperature and humidity from Si7021 sensor
  ***************************************************************************************************/
  
-void sendTempHumidityMeasurements() {
-  
-  bool forceTransmit = false;
-  int nbPulse = 1;      // 1 pulse LED si pas d'envoi (ni Temp ni Hum)
-  
-  measureCount ++;
-  if (measureCount >= FORCE_TRANSMIT_CYCLE) {
-    forceTransmit = true;  // force la transmission
-    measureCount = 0;
-  }
-
+void ReadTempAndHumidity()
+{
   // lecture sonde température et humidité   
   si7021_thc data = tempEtHumSensor.getTempAndRH();
 
-  float temperature = data.celsiusHundredths / 100.0;
+  temperature = data.celsiusHundredths / 100.0;
   DEBUG_PRINT("T: "); DEBUG_PRINTLN(temperature);
-  float diffTemp = abs(lastTemperature - temperature);
-  DEBUG_PRINT(F("TempDiff :")); DEBUG_PRINTLN(diffTemp);
-  
-  if (diffTemp >= TEMP_TRANSMIT_THRESHOLD || forceTransmit) {
-    
-    if(!send(msgTemp.set(temperature, 1),false)) {
-      DEBUG_PRINTLN(F("ERREUR envoi température"));
-    }
-    delay(DELAI_TRANS);
-    lastTemperature = temperature;
-    
-    nbPulse += 1;                    // 2 pulse si envoi Temp
-    DEBUG_PRINTLN(F("T sent!"));
-  }
 
-  int humidity = data.humidityPercent;
+  humidity = data.humidityPercent;
   DEBUG_PRINT("H: "); DEBUG_PRINTLN(humidity);
-  float diffHum = abs(lastHumidity - humidity);
-  DEBUG_PRINT(F("HumDiff  :")); DEBUG_PRINTLN(diffHum);
-  
-  if (diffHum >= HUMI_TRANSMIT_THRESHOLD || forceTransmit) {
 
-    if(!send(msgHum.set(humidity),false)) {
-      DEBUG_PRINTLN(F("ERREUR envoi humidité"));
+  // temperature = 28.2;
+
+
+  DEBUG_PRINT("sameTemperatureCounter "); DEBUG_PRINTLN(sameTemperatureCounter);
+
+  // si la sonde est en panne, on imagine que les températures mesurées seront toujours les mêmes
+  if(temperature == lastTemperatureForCounter)
+  {
+    // si le compteur atteind le seuil, delta température est 0 pour envoyer toujours la même valeur est créer une alarme du thermostat de Jeedom
+    if(sameTemperatureCounter>=SAME_TEMP_THRESHOLD)
+    {      
+      DEBUG_PRINTLN("Seuil sameTemperatureCounter atteind");
+      deltaTemperature = 0;
     }
-    delay(DELAI_TRANS);
-    lastHumidity = humidity;
-    nbPulse += 2;                    // 3 pulse si envoi Hum, 4 pulse si envoi Temp + Hum
-    DEBUG_PRINTLN("H sent!");
+    else
+    {
+      sameTemperatureCounter++;
+    }
   }
+  else
+  {
+    sameTemperatureCounter = 0;
+    deltaTemperature = nextDeltaTemperature;
+  }
+  // on mémorise la température mesurée
+  lastTemperatureForCounter = temperature;
+  
+  DEBUG_PRINT("deltaTemperature "); DEBUG_PRINTLN(deltaTemperature);
+}
 
-  pulseLed(nbPulse);   // pulse la LED
 
+
+/***************************************************************************************************
+ * Sends temperature only if necessary
+ * forceTransmit = true --> force l'envoi
+ * retour :
+ * 0 : aucun envoi effectué
+ * 1 : envoi effectué
+ ***************************************************************************************************/
+ 
+int SendTemperature(bool forceTransmit)
+{
+  float diffTemp = abs(lastTemperature - temperature);
+  DEBUG_PRINT(F("Diff. Temp. :")); DEBUG_PRINTLN(diffTemp);
+  
+  if (diffTemp >= TEMP_TRANSMIT_THRESHOLD || forceTransmit)
+  {
+    // transmission forcée et pas de changement de température, alors on ajoute offset 0,1°C pour que le thermostat de jeedom ne déclare pas la sonde en panne
+    if(diffTemp<TEMP_TRANSMIT_THRESHOLD)
+    {
+      temperature = lastTemperature + deltaTemperature;
+      nextDeltaTemperature *= -1;
+      DEBUG_PRINT(F("Diff Temp. < 0,1. nextDeltaTemperature : ")); DEBUG_PRINTLN(nextDeltaTemperature);
+    }
+    send(msgTemp.set(temperature, 1),false); delay(DELAI_TRANS);
+    lastTemperature = temperature;    
+    measureCount = 0;
+    DEBUG_PRINT(F("Temp. sent! : ")); DEBUG_PRINTLN(temperature);
+    // ajoute 1 au nombre de pulse LED à effectuer
+    return 1;
+  }
+  return 0;
+}
+
+/***************************************************************************************************
+ * Sends humidity only if necessary
+ * forceTransmit = true --> force l'envoi
+ * retour :
+ * 0 : aucun envoi effectué
+ * 2 : envoi effectué
+ ***************************************************************************************************/
+ 
+int SendHumidity(bool forceTransmit)
+{
+  float diffHum = abs(lastHumidity - humidity);
+  DEBUG_PRINT(F("Diff. Hum.  :")); DEBUG_PRINTLN(diffHum);
+  
+  if (diffHum >= HUMI_TRANSMIT_THRESHOLD || forceTransmit)
+  {
+    send(msgHum.set(humidity),false); delay(DELAI_TRANS);
+    lastHumidity = humidity;
+    measureCount = 0;
+    DEBUG_PRINT("Hum. sent! : "); DEBUG_PRINTLN(humidity);
+    // ajoute 2 au nombre de pulse LED à effectuer
+    return 2;
+  }
+  return 0;
 }
 
 
@@ -221,31 +290,22 @@ void sendTempHumidityMeasurements() {
  * envoi le niveau de la pile
  ***************************************************************************************************/
  
-void sendBatteryPercent() {
-
+void sendBatteryPercent()
+{
   batteryReportCounter ++;
 
-  if (batteryReportCounter >= BATTERY_REPORT_CYCLE) {
+  if (batteryReportCounter >= BATTERY_REPORT_CYCLE)
+  {
     uint16_t batteryVolt = readVcc();
-    DEBUG_PRINT("Battery voltage: ");
-    DEBUG_PRINT(batteryVolt);
-    DEBUG_PRINTLN(" mV");
+    DEBUG_PRINT("Battery voltage: "); DEBUG_PRINT(batteryVolt); DEBUG_PRINTLN(" mV");
     uint8_t batteryPcnt = constrain(map(batteryVolt, VMIN, VMAX, 0, 100), 0, 100);
-    DEBUG_PRINT("Battery percent: ");
-    DEBUG_PRINT(batteryPcnt);
-    DEBUG_PRINTLN(" %");
-    if(!sendBatteryLevel(batteryPcnt,false)) {
-      DEBUG_PRINTLN(F("ERREUR envoi niveau batterie"));
-    }
-    delay(DELAI_TRANS);
-    if(!send(msgBatt.set(batteryVolt),false)) {
-      DEBUG_PRINTLN(F("ERREUR envoi tension pile"));
-    }
-    delay(DELAI_TRANS);
+    DEBUG_PRINT("Battery percent: "); DEBUG_PRINT(batteryPcnt); DEBUG_PRINTLN(" %");
+    sendBatteryLevel(batteryPcnt,false); delay(DELAI_TRANS);
+    send(msgBatt.set(batteryVolt),false); delay(DELAI_TRANS);
+    DEBUG_PRINTLN(F("Battery sent!"));
     batteryReportCounter = 0;
-  }
-  
-  
+
+  }  
 }
 
 /***************************************************************************************************
@@ -255,7 +315,8 @@ void sendBatteryPercent() {
  * where internal1.1Ref = 1.1 * Vcc1 (per voltmeter) / Vcc2 (per readVcc() function)
  ***************************************************************************************************/
 
-uint16_t readVcc() {
+uint16_t readVcc()
+{
   // set the reference to Vcc and the measurement to the internal 1.1V reference
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   delay(2); // Wait for Vref to settle
@@ -275,13 +336,30 @@ uint16_t readVcc() {
  * - nombre : nombre de pulse
  ***************************************************************************************************/
 
-void pulseLed(int nombre) {
-
-  for(int i=0;i<nombre;i++) {
+void pulseLed(int nombre)
+{
+  for(int i=0;i<nombre;i++)
+  {
     digitalWrite(LED_PIN,HIGH);
     delay(DELAI_ON);
     digitalWrite(LED_PIN,LOW);
     if(nombre>1)  delay(DELAI_OFF);
   }
   
+}
+
+
+/***************************************************************************************************
+ * determine si l'envoi T. et H. doit être forcée
+ ***************************************************************************************************/
+
+bool IsForceTransmit()
+{
+  measureCount ++;
+  if (measureCount >= FORCE_TRANSMIT_CYCLE)  
+  {
+    measureCount = 0;
+    return true;     // force la transmission
+  }
+  return false;
 }
